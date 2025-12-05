210e624415494545a177ac2b63563533f0e23e40
Siam-XD <tauhidsiddique13@gmail.com>
2025-12-05 14:40:22 +0600

    cart functionality added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { IoIosStar } from "react-icons/io";
 
-const Card = ({product}) => {
+const Card = ({product, handleBuy}) => {
     const stars = [<IoIosStar className='text-amber-600' />, <IoIosStar className='text-amber-600' />, <IoIosStar className='text-amber-900' />, <IoIosStar className='text-amber-900' />, <IoIosStar className='text-amber-900' />];
   return (
     <div className='bg-slate-950 rounded-3xl flex flex-col items-center text-[#c9c9c9] pt-14 pb-10'>
@@ -9,7 +9,7 @@ const Card = ({product}) => {
         <h3>{product.name}</h3>
         <p>Price: {product.price} TK</p>
         <div className='flex text-2xl mt-5'>{stars}</div>
-        <button className='bg-indigo-500 rounded-lg text-black px-3 py-2 font-semibold mt-2'>Buy Now</button>
+        <button onClick={() => handleBuy(product)} className='bg-indigo-500 rounded-lg text-black px-3 py-2 font-semibold mt-2'>Buy Now</button>
     </div>
   )
 }
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 import Container from './Container'
+import prod1 from '../assets/prod1.png'
+import prod2 from '../assets/prod2.png'
+import prod3 from '../assets/prod3.png'
+import prod4 from '../assets/prod4.png'
+import prod5 from '../assets/prod5.png'
+import prod6 from '../assets/prod6.png'
 
-const Shop = ({products}) => {
+const products = [
+    { id: 1, name: 'Chamos Set', price: 500, src: prod1 },
+    { id: 2, name: 'K. Accessories', price: 600, src: prod2 },
+    { id: 3, name: 'Home Cooker', price: 600, src: prod3 },
+    { id: 4, name: 'Cap', price: 800, src: prod4 },
+    { id: 5, name: 'Dress', price: 700, src: prod5 },
+    { id: 6, name: 'Shoes', price: 900, src: prod6 },
+  ]
+
+const Shop = () => {
+    const [cart, setCart] = useState([])
+    const handleBuy = product => {
+        setCart([...cart, product])
+    }
   return (
     <section className='py-20'>
         <Container>
@@ -10,7 +29,7 @@ const Shop = ({products}) => {
             <div className='flex gap-12'>
                 <div className="grid grid-cols-3 gap-12 w-[70%]">
                     {products.map((product) => (
-                        <Card key={product.id} product={product} />
+                        <Card handleBuy={handleBuy} key={product.id} product={product} />
                     ))}
                 </div>
                 <div className='w-30% flex flex-col gap-6'>
@@ -21,7 +40,9 @@ const Shop = ({products}) => {
                     </div>
                     <div className='rounded bg-gray-300 p-4'>
                         <ul>
-                            <li>product</li>
+                            {cart.map((item) => (
+                                <li key={item.id}>{item.name}</li>
+                            ))}
                         </ul>
                         <button className='bg-[#ff2e78] text-white rounded-md w-full py-1 font-semibold'>Make Purchase</button>
                     </div>
